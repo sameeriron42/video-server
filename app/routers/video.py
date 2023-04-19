a7b5366b8c1b223f2dff0ea1f1ae6c9abd359b8b
sameeriron42 <sameermark42@gmail.com>
2023-04-19 20:01:34 +0530

    download video by name

--- app/routers/video.py
+++ app/routers/video.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI,Request, APIRouter, UploadFile,File
+from fastapi import FastAPI,Request, APIRouter, UploadFile,File,Form,HTTPException
 from fastapi.responses import HTMLResponse,FileResponse
 from fastapi.templating import Jinja2Templates
-import shutil
+import shutil,os
 
 router = APIRouter()
 templates = Jinja2Templates("static/templates/")
@@ -16,27 +16,30 @@ def save_upload_file(upload_file: UploadFile, destination: str) -> None:
 def uploadVideo(request: Request,file: UploadFile):
     
     if file == None:
-        return templates.TemplateResponse("index.html",{"request":request})
+        return templates.TemplateResponse("index.html",{"request":request, "response_msg":"please upload first"})
     if file.filename=='':
-        return templates.TemplateResponse("index.html",{"request":request})
+        return templates.TemplateResponse("index.html",{"request":request, "response_msg":"please upload first"})
+    if file.content_type !='video/mp4':
+        return templates.TemplateResponse("index.html",{"request":request, "response_msg":"Only Video/Mp4 supported"})
     else:
         path = 'static/videos/'+ file.filename
         
         save_upload_file(file, path)
-		#print('upload_video filename: ' + filename)
         return templates.TemplateResponse('index.html', {"request":request, "filename":file.filename})
 
 def downloadVideo(request: Request,file_name: str):
     file_location= 'static/videos/'+file_name
+    if os.path.isfile(file_location)== False:
+        return templates.TemplateResponse("index.html",{"request":request, "response_msg":"The video with the given name does not exist"})
+
     return FileResponse(path=file_location,media_type='video/mp4',filename=file_name)
 
 
-@router.post("/upload")
+@router.post("/upload",response_class=HTMLResponse)
 async def upload_video(request: Request, file: UploadFile=File(...)):
     return uploadVideo(request,file)
 
-@router.get("/download/{filename}",response_class=FileResponse)
-async def download_video(request: Request,filename: str):
-    print(filename)
+@router.post("/download",response_class=FileResponse)
+async def download_video(request: Request,filename: str =Form(...)):
     return downloadVideo(request,filename)
 
